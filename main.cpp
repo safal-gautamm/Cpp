584066d7bcdfcc985e8910f0a2cf021de4e1398b
Safal Gautam <saphal106@gmail.com>
2025-06-09 20:04:58 +0545

    small changes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 int main()
 {
     int num;
+    std::cout << time(NULL) <<std::endl;
     std::cout << "Enter the value : " ;
     std::cin >> num;
     std::cout << "You entered:" << num << std::endl;
