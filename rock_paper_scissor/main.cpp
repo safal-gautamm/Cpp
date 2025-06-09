cbb31a9a501df941b9af4c0c4545715820fbbb50
Safal Gautam <saphal106@gmail.com>
2025-06-09 19:11:54 +0545

    got data from user and computer

--- rock_paper_scissor/main.cpp
+++ rock_paper_scissor/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string get_computer_choice()
+{
+    srand(time(NULL));
+    int rand_num = rand() % 3 + 1;
+    switch (rand_num)
+    {
+    case 1:
+        return "rock";
+        break;
+    case 2:
+        return "paper";
+        break;
+    case 3:
+        return "scissors";
+    default:
+        break;
+    }
+}
+string get_user_input()
+{
+    string users;
+    cout << "Choose [Rock, Paper, Scissor] :" << endl;
+    cin >> users;
+
+    users = to_lower(users);
+    
+}
+string to_lower(string str)
+{
+    int i = 0;
+    while(str[i] != '\0')
+    {
+        str[i] = tolower(str[i]);
+        i++;
+    }
+    return str;
+}
 
 int main()
 {
-    std::cout << "Hello World !"<< std::endl;
+
+    cout << "Hello World !"<< std::endl;
 }
