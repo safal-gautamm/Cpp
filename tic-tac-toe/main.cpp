41e73800455089abf96567c128817fe7e75205e0
Safal Gautam <saphal106@gmail.com>
2025-06-21 11:57:39 +0545

    board created

--- tic-tac-toe/main.cpp
+++ tic-tac-toe/main.cpp
@@ -3,10 +3,37 @@ using namespace std;
 
 class Game
 {
-    private:
-    ...
     public:
         string name;
 
-    Game(string name) name(name){}
+    Game(string name): name(name){}
+};
+
+class Board
+{
+    public:
+    string board[9] = {"   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "};
+
+    void print_board()
+    {
+        cout << board[0] << "|" << board[1] << "|" << board[2] << endl;
+        cout << "-----------" << endl;
+        cout << board[3] << "|" << board[4] << "|" << board[5] << endl;
+        cout << "-----------" << endl;
+        cout << board[6] << "|" << board[7] << "|" << board[8] << endl;
+    }
+};
+
+int main()
+{
+    // string name1, name2;
+    // cout << "Enter the name of Player 1: ";
+    // cin >> name1;
+    // cout << "Enter the name of Player 2: ";
+    // cin >> name2;
+
+    // Game player1(name1);
+    // Game player2(name2);
+    Board board;
+    board.print_board();
 }
