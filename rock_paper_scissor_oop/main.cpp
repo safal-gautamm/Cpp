7de5b5e35f25a3d602b7789b4cbe84c5837e7758
Safal Gautam <saphal106@gmail.com>
2025-06-21 11:36:48 +0545

    Done

--- rock_paper_scissor_oop/main.cpp
+++ rock_paper_scissor_oop/main.cpp
@@ -42,11 +42,12 @@ public:
         default:
             break;
         }
+
     }
     void get_user_choice()
     {
         string user_input;
-        cout << "Enter your choice [rock, paper, scissors, exit]:" << endl;
+        cout << "\nEnter your choice [rock, paper, scissors, exit]:" << endl;
         cin >> user_input;
         toLower(&user_input);
 
@@ -58,6 +59,7 @@ public:
             this->user_choice = "scissors";
         else if (user_input == "exit" || user_input == "e")
         {
+            cout << "You scored: " << this->score << endl;
             cout<< "Exiting......" << endl;
             result = false;
         }
@@ -67,8 +69,12 @@ public:
             get_user_choice();
         }
     }
+
     void check_result()
     {
+        cout << "User's choice: "<< this->user_choice << endl ;
+        cout << "Computer's choice: " << this->computer_choice << endl ;
+
         if (this->user_choice == this->computer_choice){
             cout << "Its a draw"<<endl;
             return ;
@@ -88,11 +94,11 @@ int main()
 {
     srand(time(NULL));
     Game game;
-    while(game.result)
+    while(true)
     {
+        game.get_user_choice();
+        if(!game.result)break;
         game.get_computer_choice();
-        game.get_user_choice();
-        game.get_user_choice();
+        game.check_result();
     }
-
 }
