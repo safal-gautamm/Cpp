5a111f25edddc87422251961b5c3c236bed66166
Safal Gautam <saphal106@gmail.com>
2025-07-02 07:28:04 +0545

    workshop day4

--- Workshop/class.cpp
+++ Workshop/class.cpp
@@ -3,12 +3,22 @@ using std::cin, std::cout, std::endl, std::string;
 class Car
 {
     private:
+    static int count;
     string model;
     int capacity;
     string color;
 
     public:
     Car (string mod, int cap, string col): model(mod), capacity(cap), color(col){}
+
+    Car(Car& obj)
+    {
+        
+        model = obj.model;
+        capacity = obj.capacity;
+        color = obj.color;
+    }
+    ~Car(){count++;cout <<model << " destructed "<< count << endl ;}
 
     void start()
     {
@@ -23,11 +33,16 @@ class Car
         cout << "Engine stopped "<< endl;
     }
 };
+int Car::count = 0;
 int main()
 {
     Car car1("Land-rover", 880, "Red");
     car1.start();
     car1.details();
     car1.stop();
+    {
+        Car car2(car1);
+    }
+    cout <<"----------" <<endl;
     return 0;
 }
