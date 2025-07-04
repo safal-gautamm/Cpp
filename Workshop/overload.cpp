262b677aa62ba9693d4142c44ca20e1ad0d0247b
Safal Gautam <saphal106@gmail.com>
2025-07-04 07:33:28 +0545

    Day 6

--- Workshop/overload.cpp
+++ Workshop/overload.cpp
@@ -1,18 +1,53 @@
 #include <iostream>
 
-int  multiply(int a, int b)
+class Complex
 {
-    return a*b;
-}
-double multiply(double a, double b)
-{
-    return a*b;
-}
+    double real, image;
+
+    public:
+        Complex():real(0), image(0){}
+        Complex(double real, double image):real(real), image(image){}
+
+        Complex operator +(Complex complex)
+        {
+            Complex result;
+            result.real = real + complex.real;
+            result.image = image + complex.image;
+            return result;
+        }
+
+        Complex operator -(Complex complex)
+        {
+            Complex result;
+            result.real = real - complex.real;
+            result.image = image - complex.image;
+            return result;
+        }
+        Complex operator *(Complex complex)
+        {
+            Complex result;
+            result.real = real * complex.real - image * complex.image;
+            result.image = real * complex.image + image * complex.real;
+            return result;
+        }
+
+        friend std::ostream& operator<<(std::ostream& out, Complex complex)
+        {
+            std::cout << complex.real << " + " <<complex.image << "i "<< std::endl;
+            return out;
+        }
+};
+
 int main()
 {
-    int a = 10, b = 11;
-    double c = 1.01, d = 13.07;
-    std::cout << "10 x 11 = " << multiply(a, b) << std::endl;
-    std::cout << "1.01 x 13.07 = " << multiply(c, d) << std::endl;
-    return 0;
+    Complex c1(10, 11);
+    Complex c2(12, 13);
+    std::cout << c1;
+    std::cout << c2;
+    Complex c3 = c1 + c2;
+    std::cout << c3;
+    Complex c4 = c1 - c2;
+    std::cout << c4;
+    Complex c5 = c1 * c2;
+    std::cout << c5;
 }
